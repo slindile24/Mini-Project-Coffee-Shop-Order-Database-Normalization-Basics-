bfedd7f9922d358361b01271964ab1c616528bf0
Slindile <slindilenkabs23@gmail.com>
2026-01-30 15:11:31 +0200

    merged tables according to id

--- Normalization/normalization.py
+++ Normalization/normalization.py
@@ -49,7 +49,7 @@ orders_df = pd.DataFrame(messy_data)
 
 """""
 
-def create_tables():
+def create_tables(orders_df):
     customers_df = orders_df[["customer_name","customer_phone"]].drop_duplicates().reset_index(drop=True)
     customers_df["customer_id"] = customers_df.index + 1
 
@@ -59,10 +59,28 @@ def create_tables():
     baristas_df = orders_df[["barista_name"]].drop_duplicates().reset_index(drop=True)
     baristas_df["barista_id"] = baristas_df.index + 1
 
-    
+
+
+    return customers_df,drinks_df,baristas_df
+
+def normalization(orders_df,customers_df,drinks_df,baristas_df):
+   
+    orders_normalized = orders_df.merge(customers_df, on=["customer_name", "customer_phone"])
+    orders_normalized = orders_normalized.merge(drinks_df, on=["drink_name", "drink_size", "drink_price"])
+    orders_normalized = orders_normalized.merge(baristas_df, on="barista_name")
+
+    orders_finalized = orders_normalized[
+        ["order_id","customer_id","drink_id","barista_id"]
+    ]
+
+    return orders_finalized
+
+customers_df, drinks_df, baristas_df = create_tables(orders_df)
+
+print(normalization(orders_df, customers_df, drinks_df, baristas_df))
 
 
 
-    return f"{customers_df} \n {drinks_df} \n {baristas_df}"
-print(create_tables())
+
+
 
